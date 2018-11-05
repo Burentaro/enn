132cfea4b520ccf9dcddff5bc16a1b52ac75b335
DESKTOP-PBOD351\Thilo <thilo.kernchen@gmail.com>
2018-11-06 00:04:28 +0900

    no message

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .models import MenuItem
+
 # Create your views here.
+
+def menu_item_detail(request):
+
+	obj = MenuItem.objects.get(id=1)
+
+	return render(request, 'menu/detail.html', {})
