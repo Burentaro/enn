e56e8d6b4c95add635ffa586d92f44c8a9e4e00c
VRQA-PC-4\VRQA_4 <thilo@asadigital.net>
2018-11-05 18:51:33 +0900

    no message

---
+++ menu/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+
+class MenuItem(models.Model):
+	name = models.CharField(max_length=120)
+	category = models.CharField(
+		max_length=120,
+		choices=[('Drink', 'Drink'), ('Food', 'Food')])
+	description = models.TextField()
+	price = models.DecimalField(decimal_places=0, max_digits=1000)
+	featured = models.BooleanField()
+
+	def __str__(self):
+		return self.name
---
+++ pages/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+
+from menu.models import MenuItem
+
+# Create your views here.
+
+
+def home(request):
+
+	context = {
+		'title': 'ホーム'
+	}
+
+	return render(request, 'pages/home.html', context)
+
+
+def menu(request):
+
+	context = {
+		'items': MenuItem.objects.all(),
+		'title': 'メニュ',
+	}
+
+	return render(request, 'pages/menu.html', context)
+
+
+def greeting(request):
+
+	context = {
+		'title': '挨拶'
+	}
+
+	return render(request, 'pages/greeting.html', context)
+
+
+def courses(request):
+
+	context = {
+		'title': 'コース'
+	}
+
+	return render(request, 'pages/courses.html', context)
